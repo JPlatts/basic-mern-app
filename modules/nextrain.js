8c59d745afaa820c7f56bcd19fde8bdea3f4f42a
JPlatts <japlatts@gmail.com>
2022-07-18 14:43:53 -0400

    nextrain wip

--- modules/nextrain.js
+++ modules/nextrain.js
@@ -14,40 +14,68 @@ async function nextrain(stations) {
     return eps;
   },[]); 
 
-  
+
   let trainData = [];
-  await endpoints.forEach(async ep => {
-    let nd = await getData(ep);
+  for (let i = 0; i < endpoints.length; i++) {
+    let f = await getData(endpoints[i]);
+    trainData = trainData.concat(f);
+  }
+ 
+  let stationUpdates = []
+  stations.forEach(s => {
+    
+    let tt = `${s.gtfsCode}N`;
+    let tts = `${s.gtfsCode}S`;
+    
+    let uptownTimes = trainData.filter(e => e.tripUpdate && e.tripUpdate.stopTimeUpdate.some(s => s.stopId ==  tt )).map(e => {
+      return {
+        route: e.tripUpdate.trip.routeId,
+        stops: e.tripUpdate.stopTimeUpdate.filter(s => s.stopId == tt).map(s => {
+          let d = new Date(s.arrival.time.low * 1000);
+          return d.toLocaleString('en');
+        })
+      }
+    });
+    
 
-    trainData = trainData.concat(nd);
-  });
+    let downtownTimes = trainData.filter(e => e.tripUpdate && e.tripUpdate.stopTimeUpdate.some(s => s.stopId == tts)).map(e => {
+      return {
+        route: e.tripUpdate.trip.routeId,
+        stops: e.tripUpdate.stopTimeUpdate.filter(s => s.stopId == tts).map(s => {
+          let d = new Date(s.arrival.time.low * 1000);
+          return d.toLocaleString('en');
+        })
+      }
+    });
+    
+    stationUpdates.push({station: s, uptownTimes: uptownTimes, downtownTimes: downtownTimes});
+  }); 
 
-  function getData(ep) {
-    let chunks = [];
-    let root = protobuf.loadSync('./metadata/gtfs-realtime.proto');
-    let feedMessage = root.lookupType('transit_realtime.FeedMessage');
-    return new Promise((resolve) => {
-      https.get(ep, { 
-        headers: { "x-api-key": MTA_KEY} },
-        (resp) => {
-          resp.on('data', (chunk) => {
-            chunks.push(chunk)
-          });
-          resp.on('end', () => {
-            let data = Buffer.concat(chunks);
-            let entities = feedMessage.decode(data).entity;
-             resolve(entities);
-          });
-       });
+
+
+  function getData(endpoint) {
+    return new Promise(resolveIt => {
+      let chunks = [];
+      let root = protobuf.loadSync('./metadata/gtfs-realtime.proto');
+      let feedMessage = root.lookupType('transit_realtime.FeedMessage');
+      https.get(endpoint, { 
+      headers: { "x-api-key": MTA_KEY} },
+      (resp) => {
+        resp.on('data', (chunk) => {
+          chunks.push(chunk)
+        });
+        resp.on('end', () => {
+          let data = Buffer.concat(chunks);
+          let entities = feedMessage.decode(data).entity;
+          resolveIt(entities.filter(e => e.tripUpdate && e.tripUpdate.stopTimeUpdate));
+        });
+      });
+    
     });
   }
-
-  console.log(trainData);
-  return trainData;;
-
   
-    
-  
+  return stationUpdates;
+   
 }
 
 module.exports = nextrain;
--- routes/nextrains.js
+++ routes/nextrains.js
@@ -11,44 +11,12 @@ const { MTA_KEY } = require('../modules/config');
 
 router.get('/', async (req, res) => {
   try {
-
     let u = await req.user.populate('stations');
     let nts = await nextrain(u.stations);
-
-    let chunks = [];
-    let root = protobuf.loadSync('./metadata/gtfs-realtime.proto');
-    let feedMessage = root.lookupType('transit_realtime.FeedMessage');
-    
-    https.get("https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-ace", { 
-      headers: { "x-api-key": MTA_KEY} },
-      (resp) => {
-        resp.on('data', (chunk) => {
-          chunks.push(chunk)
-        });
-        resp.on('end', () => {
-          let data = Buffer.concat(chunks);
-          let entities = feedMessage.decode(data).entity;
-          let b = entities.filter(e => e.tripUpdate && e.tripUpdate.stopTimeUpdate.some(s => s.stopId == 'A11S')).map(e => {
-            return {
-              route: e.tripUpdate.trip.routeId,
-              stops: e.tripUpdate.stopTimeUpdate.filter(s => s.stopId == 'A11S').map(s => {
-                let d = new Date(s.arrival.time.low * 1000);
-                return d.toLocaleString('en');
-              })
-            }
-          });
-          
-          res.status(200).json({ msg:'Success', nextrains: nts  });
-        });
-      }).on("error", (err) => {
-        console.log("Error: " + err.message);
-        res.status(400).json({ msg:'Invalid Request' });
-      });
-    
-    
+    res.status(200).json({ msg:'Success', nextrains: nts  });
+  
   } catch (err) {
     console.log(err);
-    
   }
 });
 
