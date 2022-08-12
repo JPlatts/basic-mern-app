57b8b979526b10f2290b7f8912760ad4a888c4d9
JPlatts <japlatts@gmail.com>
2022-08-12 17:56:12 -0400

    drunken refactoer

--- client/src/features/nextrains/Nextrain.jsx
+++ client/src/features/nextrains/Nextrain.jsx
@@ -3,30 +3,16 @@ import { useDispatch } from 'react-redux'
 import { deleteStation } from './nextrainSlice'
 import TrainImage from './TrainImage'
 import Spinner from '../../app/Spinner';
+import { fmtStopDate, getMins } from '../../modules/common';
 
 function Nextrain(props) {
   
   const dispatch = useDispatch();
-  
-  const fmtStopDate = (d) => {
-    let dt = new Date(d);
-    var hours = dt.getHours();
-    var minutes = dt.getMinutes();
-    var ampm = hours >= 12 ? 'pm' : 'am';
-    hours = hours % 12;
-    hours = hours ? hours : 12; // the hour '0' should be '12'
-    minutes = minutes < 10 ? '0'+minutes : minutes;
-    return hours + ':' + minutes + ' ' + ampm;
-  }
-
-  const getMins = (d) => {
-    let dt = new Date(d);
-    return parseInt(Math.abs(dt.getTime() - new Date().getTime()) / (1000 * 60) % 60);
-  }
 
   const deleteMe = () => {
     dispatch(deleteStation(props.nextrain.station));
   }
+  
   return (
     <div className="container">
       <div className="card">
--- client/src/modules/common.js
+++ client/src/modules/common.js
@@ -1,10 +1,29 @@
 const validateEmail = (email) => {
-    return String(email).toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+  return String(email).toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
 }
 
 const validatePassword = (password) => {
-    let pwRegex = /(?=^.{8,}$)(?=.*\d)(?=.*[!@#$%^&*]+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/;
-    return pwRegex.test(password);
+  let pwRegex = /(?=^.{8,}$)(?=.*\d)(?=.*[!@#$%^&*]+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/;
+  return pwRegex.test(password);
 }
 
-export { validateEmail, validatePassword } 
+const padTime = (n) => {
+    return n < 10 ? `0${n}` : n;
+};
+
+const fmtStopDate = (d) => {
+  let dt = new Date(d);
+  let h = dt.getHours();
+  let m = padTime(dt.getMinutes());
+  let s = padTime(dt.getSeconds());
+  let a = h >= 12 ? 'PM' : 'AM';
+  h = h % 12;
+  h = padTime(h ? h : 12); // the 0 should be 12
+  return `${h}:${m}:${s} ${a}`;
+}
+
+const getMins = (d) => {
+  let dt = new Date(d);
+  return parseInt(Math.abs(dt.getTime() - new Date().getTime()) / (1000 * 60) % 60);
+}
+export { validateEmail, validatePassword, fmtStopDate, getMins } 
