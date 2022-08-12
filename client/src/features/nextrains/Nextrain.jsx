ff7b9d36bcc8315893d7c302487b0c3ec4d85f07
JPlatts <japlatts@gmail.com>
2022-08-12 09:25:08 -0400

    Nextrain layout

--- client/src/features/nextrains/Nextrain.jsx
+++ client/src/features/nextrains/Nextrain.jsx
@@ -10,39 +10,38 @@ function Nextrain(props) {
   const deleteMe = () => {
     dispatch(deleteStation(props.nextrain.station));
   }
-    console.log(props)
   return (
     <div className="container">
-      <div className="columns">
-        <div className="column is-three-fifths is-offset-one-fifth">
-          <article className="panel is-info">
-            <div className="panel-heading">
-              <div className="columns is-mobile">
-                <div className="column is-11">
-                  {props.nextrain && props.nextrain.station.name} - {props.nextrain && props.nextrain.station.line}
-                </div>
-                <div className="column is-1">
-                  <FaTrashAlt onClick={deleteMe} />
-                </div>  
+      <div className="card">
+        <header className="card-header">
+          <span className="card-header-title">{props.nextrain && props.nextrain.station.name} - {props.nextrain && props.nextrain.station.line}</span>
+          <button className="card-header-icon">
+            <span className="icon" title="Remove station">
+              <FaTrashAlt onClick={deleteMe} />
+            </span>
+          </button>
+        </header>
+        <div className="card-content">
+          <div className="content">
+            <div className="columns">
+              <div className="column is-half">
+                <table className="table">
+                  <tr>
+                    <th colSpan="2">{props.nextrain && props.nextrain.station.northLabel}</th>
+                  </tr>
+                  {props.nextrain.uptownTimes.map((n) => ( <tr key={`${n.stops}${n.route.route}`}><td><TrainImage route={n.route} /></td><td>{n.stops[0]}</td></tr>))}
+                </table>
+              </div>
+              <div className="column is-half">
+                <table className="table">
+                  <tr>
+                    <th colSpan="2">{props.nextrain && props.nextrain.station.southLabel}</th>
+                  </tr>
+                  {props.nextrain.downtownTimes.map((n) => ( <tr key={`${n.stops}${n.route.route}`}><td><TrainImage route={n.route} /></td><td>{n.stops[0]}</td></tr>))}
+                </table>
               </div>
             </div>
-            <div className="panel-block">
-              <div className="tile is-ancestor">
-                <div className="tile is-half"><h3>{props.nextrain && props.nextrain.station.northLabel}</h3></div>
-                <div className="tile"><h3>{props.nextrain && props.nextrain.station.southLabel}</h3></div>
-              </div>
-            </div>
-            <div className="panel-block">
-              <div className="tile is-ancestor">
-                <div className="tile is-vertical is-6">
-                  {props.nextrain.uptownTimes.map((n) => ( <p key={`${n.stops}${n.route.route}`}><TrainImage route={n.route} /> {n.stops[0]}</p>))}
-                </div>
-                <div className="tile is-vertical is-6">
-                  {props.nextrain.downtownTimes.map((n) => ( <p key={`${n.stops}${n.route.route}`}><TrainImage route={n.route} /> {n.stops[0]}</p>))}
-                </div>
-              </div>
-            </div>
-          </article>
+          </div>
         </div>
       </div>
     </div>
